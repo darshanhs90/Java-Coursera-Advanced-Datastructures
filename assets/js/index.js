1f5dad18b1041b488b416a8b82db96cbcf5782ba
Haridarshan HS <hsdars@gmail.com>
2015-07-21 23:58:07 -0500

    bash added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 $(function(){
         $("#typed").typed({
-            strings: ["I am Haridarshan.","I am a Java Developer","I am a JavaScript Developer","I am a Android Developer"],
+            strings: ["I am a Hackathon Hacker","I am a Java Developer","I am a JavaScript Developer","I am a Android Developer","I am a FullStack Developer"],
             typeSpeed: 30,
             backDelay: 100,
             loop: false,
             contentType: 'text', // or text
             // defaults to false for infinite loop
-            loopCount: false,
+            loopCount: 2,
             callback: function(){ foo(); },
             resetCallback: function() { newTyped(); }
         });
