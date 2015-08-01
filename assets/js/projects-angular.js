7ec2c69340f9ed4c4fec06a53f50e9794edc6ddf
Haridarshan HS <hsdars@gmail.com>
2015-08-01 14:50:23 -0500

    projects.html modified

--- assets/js/projects-angular.js
+++ assets/js/projects-angular.js
@@ -1,8 +1,15 @@
 var app=angular.module('myApp',[]);
 app.controller('myCtrl',function($scope,$http) {
-$('#rw1').css('visibility','hidden');
+	$('#rw1').css('visibility','hidden');
+	wheel = new wheelnav('wheelDiv');
+	wheel.slicePathFunction = slicePath().MenuSlice;
+	wheel.spreaderPathFunction = spreaderPath().HolderSpreader;
+	wheel.clickModeRotate = true;
+	wheel.colors = ['#AB274F','lightBlue','lightGreen'];
+	wheel.spreaderEnable = true;
+	wheel.createWheel(['Repo', 'Dev Post', 'holder']);
 	$scope.project=function($val){
-			
+
 
 		$('#one').css('background-image','url(pages/'+$val+'1.jpg)');
 		$('#two').css('background-image','url(pages/'+$val+'2.jpg)');
